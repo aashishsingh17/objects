65a56d9fdfe313f1298385e35d2b15de82b9c001
Aashish Kumar Singh <aashish.singh.17@mountblue.tech>
2021-08-17 13:58:34 +0530

    for MapObject.js

--- test/testMapObject.js
+++ test/testMapObject.js
@@ -1,5 +1,9 @@
-import {mapObject,cb} from '../mapObject.js'
+import {mapObject} from '../mapObject.js'
 import { testObject } from './testObject.js'
 let res=[];
 res=mapObject(testObject,cb);
 console.log(res);
+function cb(x)
+ {
+        return x = x + "z";
+ }
