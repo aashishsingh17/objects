19d30f145e6f90cd8f24ae663de9cb7ba8135a58
Aashish Kumar Singh <aashish.singh.17@mountblue.tech>
2021-08-17 13:59:27 +0530

    for mapObject

--- mapObject.js
+++ mapObject.js
@@ -1,19 +1,12 @@
 function mapObject(obj, cb) {
-          // Like map for arrays, but for objects.
-          // Transform the value of each property in 
-          //turn by passing it to the callback function.
-          // http://underscorejs.org/#mapObject
           let arr = [];
           for(var i in obj)
           {
-              var x= obj[i];
-              cb(x);
-             arr.push(x);
+              var x=cb(obj[i]);
+              arr.push(x);
+             // console.log(obj[i]);
           }
           return arr;
       }
- function cb(x)
- {
-        x = x + "z";
- }
- export{mapObject,cb}
+ 
+ export{mapObject}
